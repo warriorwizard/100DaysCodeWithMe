46bbe57823b2f10ab52fdc7612de7e9c7dc37b2c
Amit Yadav <amit375395yadav@gmail.com>
2022-06-22 09:29:49 +0530

    Add files via upload

---
+++ java_1D_ARRAY.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+    
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int[] list=new int[n];
+    
+    for (int i=0; i<n;i++){
+        list[i]=sc.nextInt();
+    }
+    for (int i=0;i<list.length;i++){
+        System.out.println(list[i]);
+    }
+    
+    
+    
+    
+    }
+}
